9846e91706a4c27f85c2f31fdb37ed98a00dead7
marvelpokemaster <72781128+marvelpokemaster@users.noreply.github.com>
2024-05-02 23:46:04 +0530

    Create sql_code.sql

---
+++ sql_code.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS mydatabase;
+CREATE DATABASE mydatabase;
+
+USE mydatabase;
+
+CREATE TABLE mytable (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(50),
+    password VARCHAR(50),
+    name VARCHAR(50),
+    roll VARCHAR(50),
+    school VARCHAR(50),
+    specialisation VARCHAR(50),
+    fee_initial INT,
+    sgpa1 INT,
+    sgpa2 INT,
+    lib DATE,
+    indiscipline INT
+    
+);
