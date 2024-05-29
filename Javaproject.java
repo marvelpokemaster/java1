e7491ccb8f5301a4dd76879cd2a1988a397c4f35
marvelpokemaster <72781128+marvelpokemaster@users.noreply.github.com>
2024-05-29 21:37:40 +0530

    Update Javaproject.java

--- Javaproject.java
+++ Javaproject.java
@@ -1,9 +1,8 @@
 package com.mycompany.javaproject;
 
-import java.sql.*;
 import java.util.*;
-public class Javaproject {
 
+public class Javaproject {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -11,88 +10,70 @@ public class Javaproject {
         System.out.print("ENTER YOUR SCHOOL: ");
         String school = sc.nextLine();
 
-        Amrita student = null;
-
         if (school.equalsIgnoreCase("CSE") || school.equalsIgnoreCase("CSEAI") ||
-                school.equalsIgnoreCase("CSECS") || school.equalsIgnoreCase("CSEDA") ||
-                school.equalsIgnoreCase("ELC") || school.equalsIgnoreCase("EEE") ||
-                school.equalsIgnoreCase("RA") || school.equalsIgnoreCase("RAI")) {
-
+            school.equalsIgnoreCase("CSECS") || school.equalsIgnoreCase("CSEDA")) {
             System.out.print("ENTER YOUR ROLL NUMBER: ");
             String roll_no = sc.nextLine();
-            System.out.print("ENTER YOUR PASSWORD: ");
+            System.out.print("Enter password: ");
             String password = sc.nextLine();
             System.out.print("Enter SGPA1: ");
             double sgpa1 = sc.nextDouble();
             System.out.print("Enter SGPA2: ");
             double sgpa2 = sc.nextDouble();
-            System.out.print("Enter CGPA: ");
-            double cgpa = sc.nextDouble();
+            System.out.print("Enter beforeFee: ");
+            double beforeFee = sc.nextDouble();
             sc.nextLine();
 
+            Amrita student = null;
             switch (school.toUpperCase()) {
                 case "CSE":
-                    student = new CSE(roll_no, password, "Amrita School of Computing", school, sgpa1, sgpa2, cgpa);
+                    student = new CSE(roll_no, password, "Amrita School of Computing", school, sgpa1, sgpa2, beforeFee);
                     break;
                 case "CSEAI":
-                    student = new CSEAI(roll_no, password, "Amrita School of Computing", school, sgpa1, sgpa2, cgpa);
+                    student = new CSEAI(roll_no, password, "Amrita School of Computing", school, sgpa1, sgpa2, beforeFee);
                     break;
                 case "CSECS":
-                    student = new CSECS(roll_no, password, "Amrita School of Computing", school, sgpa1, sgpa2, cgpa);
+                    student = new CSECS(roll_no, password, "Amrita School of Computing", school, sgpa1, sgpa2, beforeFee);
                     break;
                 case "CSEDA":
-                    student = new CSEDA(roll_no, password, "Amrita School of Computing", school, sgpa1, sgpa2, cgpa);
+                    student = new CSEDA(roll_no, password, "Amrita School of Computing", school, sgpa1, sgpa2, beforeFee);
                     break;
+            }
+            student.calculateFee();
+        } else if (school.equalsIgnoreCase("ELC") || school.equalsIgnoreCase("EEE") ||
+            school.equalsIgnoreCase("RA") || school.equalsIgnoreCase("RAI")) {
+            System.out.println("WELCOME TO AMRITA SCHOOL OF ENGINEERING");
+            System.out.print("ENTER YOUR ROLL NUMBER: ");
+            String roll_no = sc.nextLine();
+            System.out.print("Enter password: ");
+            String password = sc.nextLine();
+            System.out.print("Enter SGPA1: ");
+            double sgpa1 = sc.nextDouble();
+            System.out.print("Enter SGPA2: ");
+            double sgpa2 = sc.nextDouble();
+            System.out.print("Enter beforeFee: ");
+            double beforeFee = sc.nextDouble();
+            sc.nextLine();
+
+            Amrita student = null;
+            switch (school.toUpperCase()) {
                 case "ELC":
-                    student = new ELC(roll_no, password, "Amrita School of Engineering", school, sgpa1, sgpa2, cgpa);
+                    student = new ELC(roll_no, password, "Amrita School of Engineering", school, sgpa1, sgpa2, beforeFee);
                     break;
                 case "EEE":
-                    student = new EEE(roll_no, password, "Amrita School of Engineering", school, sgpa1, sgpa2, cgpa);
+                    student = new EEE(roll_no, password, "Amrita School of Engineering", school, sgpa1, sgpa2, beforeFee);
                     break;
                 case "RA":
-                    student = new RA(roll_no, password, "Amrita School of Engineering", school, sgpa1, sgpa2, cgpa);
+                    student = new RA(roll_no, password, "Amrita School of Engineering", school, sgpa1, sgpa2, beforeFee);
                     break;
                 case "RAI":
-                    student = new RAI(roll_no, password, "Amrita School of Engineering", school, sgpa1, sgpa2, cgpa);
+                    student = new RAI(roll_no, password, "Amrita School of Engineering", school, sgpa1, sgpa2, beforeFee);
                     break;
-                default:
-                    System.out.println("Invalid school entered.");
-                    return;
             }
-
-            try {
-                DatabaseHelper.saveStudentToDatabase(student);
-                student.calculateFee();
-                DatabaseHelper.updateFee(student.getRollNo(), student.getFee());
-                display(roll_no);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
+            student.calculateFee();
         } else {
             System.out.println("YOU ARE NOT A STUDENT OF AMRITA SCHOOL");
         }
-
         sc.close();
-    }
-
-    static void display(String roll_no) {
-        try {
-            ResultSet rs = DatabaseHelper.selectRow(roll_no);
-            if (rs != null && rs.next()) {
-                System.out.println("Roll No: " + rs.getString("roll_no"));
-                System.out.println("Password: " + rs.getString("password"));
-                System.out.println("School: " + rs.getString("school"));
-                System.out.println("Branch: " + rs.getString("branch"));
-                System.out.println("SGPA1: " + rs.getDouble("sgpa1"));
-                System.out.println("SGPA2: " + rs.getDouble("sgpa2"));
-                System.out.println("CGPA: " + rs.getDouble("cgpa"));
-                System.out.println("Fee: " + rs.getDouble("fee"));
-            } else {
-                System.out.println("No student found with roll number: " + roll_no);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
