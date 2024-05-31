c142d5bd8dffb4edb50ef5287c4082f29ad7c16b
marvelpokemaster <marvelpokemaster@gmail.com>
2024-06-01 00:53:30 +0530

    added sql (with afterfee)

---
+++ sql_code.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE AmritaDB;
+
+USE AmritaDB;
+
+CREATE TABLE students (
+    roll_no VARCHAR(50) PRIMARY KEY,
+    password VARCHAR(50),
+    school VARCHAR(50),
+    branch VARCHAR(50),
+    sgpa1 DOUBLE,
+    sgpa2 DOUBLE,
+    beforefee DOUBLE,
+    afterfee DOUBLE
+);
